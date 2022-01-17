34959e6b444765f2c5269ad3afe4d29a7e173daa
DreamSky1996 <webhipe@gmail.com>
2022-01-17 13:57:27 +0900

    add rinkeby config

--- hardhat.config.js
+++ hardhat.config.js
@@ -45,6 +45,12 @@ module.exports = {
       chainId: 43114,
       gasPrice: 30000000000,
       accounts: [privateKey]
+    },
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      chainId: 4,
+      gasPrice: 30000000000,
+      accounts: [privateKey]
     }
   }
 };
